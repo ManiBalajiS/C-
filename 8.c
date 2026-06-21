a316762db242ae40454085eb6bfe9c85adf36929
MANI S <karnanmsb777@gmail.com>
2026-06-21 18:54:18 +0530

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorial(int n)
+{
+   int c;
+   if(n==0)
+      return 1;
+   else
+      c=n*factorial(n-1);
+   return c;
+}
+int main()
+{
+   int f,n;
+   printf("Enter a number..");
+   scanf("%d",&n);
+   f=factorial(n);
+   printf("\n Factorial of %d is %d",n,f);
+}
