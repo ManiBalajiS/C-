d3f7eaa48c1028197d06aaf6f8499cb432b4343b
MANI S <karnanmsb777@gmail.com>
2026-06-21 19:22:49 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int n;
+   printf("Enter a number to check..");
+   scanf("%d",&n);
+   if (n%2==0)
+      printf("n is Even.");
+   else
+      printf("n is Odd.");
+   return 0;
+}   
