ea5121fc14c9a9b8b1338ce8d18c706d6409d6fc
MANI S <karnanmsb777@gmail.com>
2026-06-21 19:27:25 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,18 @@
+echo “Enter 3 Subject marks of a student:”
+read s1 s2 s3
+sum1=`expr $s1 + $s2 + $s3`
+echo “Sum of 3 subjects are:” $sum1
+per=`expr $sum1 / 3`
+echo “Percentage is :” $per
+if [ $per -ge 75 ]
+then
+      echo “Distinction”
+elif [ $per -ge 60 -a $per -lt 75  ]
+then
+      echo “First Class”
+elif [ $per -ge 40 -a $per -lt 59 ]
+then
+      echo “Second Class”
+else
+      echo “Fail”
+fi
